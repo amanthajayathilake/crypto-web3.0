68df154fcacbbf447dd99a14c6bfa106ce7115c1
amanthajayathilake <amanthajayathilake@gmail.com>
2022-01-14 10:44:29 +0530

    changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -42,6 +42,7 @@ const Navbar = () => {
                     className="z-10 fixed -top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
                     flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in"
                 >
+                    {/* blue-glassmorphism color */}
                 <li className="text-xl w-full my-2"><AiOutlineClose onClick={() => setToggleMenu(false)} /></li>
                 {[
                     <a href='#testimonials' className='page-scroll'>Market</a>, 
